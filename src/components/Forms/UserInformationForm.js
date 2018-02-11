86ea6465bdaeb2efe1b61bde03ac6fceb3dc0ad6
Arnaud Augustin <arnauda@bam.tech>
2018-02-11 11:24:15 +0100

    Add the touchable submit button in the form

--- src/components/Forms/UserInformationForm.js
+++ src/components/Forms/UserInformationForm.js
@@ -1,10 +1,10 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { reduxForm, Field } from 'redux-form';
-import { ScrollView, Text, StyleSheet } from 'react-native';
+import { ScrollView, Text, StyleSheet, TouchableOpacity } from 'react-native';
 
 import SimpleTextInput from './SimpleTextInput';
 
-function UserInformationForm() {
+const UserInformationForm = (props) => {
   return (
     <ScrollView keyboardShouldPersistTaps={'handled'}>
       <Text style={styles.fieldNameStyle}>Username</Text>
@@ -12,15 +12,27 @@ function UserInformationForm() {
         name={'username'}
         component={SimpleTextInput}
       />
-    </ScrollView>
-  );
-}
+      <TouchableOpacity style={styles.touchableStyle} onPress={props.handleSubmit}>
+        <Text style={styles.touchableTextStyle}>Submit!</Text>
+      </TouchableOpacity >
+    </ScrollView >)
+};
 
 const styles = StyleSheet.create({
   fieldNameStyle: {
     marginHorizontal: 20,
     marginTop: 10,
-  }
+  },
+  touchableStyle: {
+    marginTop: 40,
+    alignSelf: 'center',
+    borderWidth: 1,
+    borderRadius: 20,
+    padding: 10,
+  },
+  touchableTextStyle: {
+    fontSize: 30,
+  },
 })
 
-export default reduxForm({ form: 'signIn' })(UserInformationForm);
+export default reduxForm({ form: 'signUp' })(UserInformationForm);
