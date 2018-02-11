e04aca634eda727ced1f5aba536bee9f33495979
Arnaud Augustin <arnauda@bam.tech>
2018-02-11 11:24:40 +0100

    Add the formReducer in the rootReducer

--- src/modules/reducers.js
+++ src/modules/reducers.js
@@ -1,4 +1,5 @@
 import { combineReducers } from 'redux';
+import { reducer as formReducer } from 'redux-form';
 
 import { appReducer } from './App';
 import { navReducer } from './Nav';
@@ -6,6 +7,7 @@ import { navReducer } from './Nav';
 const rootReducer = combineReducers({
   app: appReducer,
   nav: navReducer,
+  form: formReducer,
 });
 
 export default rootReducer;
